be4433565fdf3931a68f2811d84232e86c343657
Elena Starikova <elena.g.starikova@yandex.ru>
2023-01-16 08:43:12 +0700

    неработающая лента

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
+
 function initCarousel() {
-  // ваш код...
+  let carouselInner = document.querySelector(".carousel__inner");
+  let carousel = document.querySelector(".carousel");
+  let shift = 300;
+  carousel.addEventListener("click", function(event) {
+
+    for (node of carousel.child(Nodes)) {
+      if (event.target.contains(".carousel__arrow") !== "true") return;
+
+      if (event.target.classList.contains(".carousel__arrow" && ".carousel__arrow_right") === "true") {
+        carouselInner.style.transform = 'translateX(-shift + "px")';
+         if (shift === 900) {
+          event.target.style.display = "none"
+        }
+        shift += shift
+      };
+
+      if (event.target.classList.contains(".carousel__arrow" && ".carousel__arrow_left") === "true") {
+        carouselInner.style.transform = 'translateX(shift + "px")';
+        
+        if (shift === 900) {
+          event.target.style.display = "none"
+        }
+        shift += shift
+      };
+     
+    }
+  })
 }
